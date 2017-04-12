9afb2607bdb64808b10f3044340152a22bac4041
Błażej Owczarczyk <blazej.owczarczyk@gmail.com>
2017-04-12 13:30:07 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# HTTP auth proxy
+
+Proxies http requests through an authenticated channel.
+
+## Run
+
+To start the server type `npm start` and provide target url, username and password.
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const request = require('request');
+const cors = require('cors')
+const readlineSync = require('readline-sync');
+
+const port = process.env.PORT || 3001;
+const host = readlineSync.question('Host (without trailing slash): ');
+const username = readlineSync.question('HTTP auth username: ');
+const password = readlineSync.question('HTTP auth password: ', {
+  hideEchoBack: true,
+});
+console.log('Thank you. Server is now listening on port ' + port);
+
+const app = express();
+app.use(cors());
+app.use('/', function(req, res) {
+  const url = host + req.url;
+
+  console.log(url);
+
+  const authData = {
+    auth: {
+      username: username,
+      password: password,
+    }
+  };
+
+  req.pipe(request(url, authData)).pipe(res);
+});
+
+app.listen(port);
